4b983f72dd25b993873e693ca0aed71608381a62
Louis Brennan <louis.brennan1@ucdconnect.ie>
2026-03-05 12:32:39 +0000

    Search bar

--- src/main/java/com/example/marketplace/controller/HomeController.java
+++ src/main/java/com/example/marketplace/controller/HomeController.java
@@ -5,6 +5,7 @@ import com.example.marketplace.repository.ProductRepository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import static com.example.marketplace.constant.Genres.F_GENRES;
 import static com.example.marketplace.constant.Genres.NF_GENRES;
@@ -21,6 +22,14 @@ public class HomeController {
     @GetMapping({"/", "/home", "/index"})
     public String home(Model model) {
         model.addAttribute("products", productRepository.findAll());
+        model.addAttribute("f_genres", F_GENRES);
+        model.addAttribute("nf_genres", NF_GENRES);
+        return "index";
+    }
+
+    @GetMapping("/search")
+    public String index(Model model, @RequestParam(required = false) String keyword) {
+        model.addAttribute("products", productRepository.search(keyword).orElse(null));
         model.addAttribute("f_genres", F_GENRES);
         model.addAttribute("nf_genres", NF_GENRES);
         return "index";
--- src/main/java/com/example/marketplace/repository/ProductRepository.java
+++ src/main/java/com/example/marketplace/repository/ProductRepository.java
@@ -2,8 +2,21 @@ package com.example.marketplace.repository;
 
 import com.example.marketplace.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query(
+            "SELECT p from Product p WHERE " +
+            "LOWER(p.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.author) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.publisher) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.genre) LIKE LOWER(CONCAT('%', :keyword, '%'))"
+    )
+    public Optional<List<Product>> search(String keyword);
 }
